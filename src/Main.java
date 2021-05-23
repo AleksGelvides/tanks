08a9d0b5998a47aba5a555e892fe55971fd04c8f
Алексей <gelvides.aleksey@outlook.com>
2021-05-23 17:28:03 +0700

    Заработало

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        Tank tank = new Tank();
-        tank.goForward(10);
-        tank.printPosition();
-        tank.turnRight();
-        tank.goForward(50);
-        tank.printPosition();
-        tank.turnLeft();
-        tank.goBackward(100);
-        tank.printPosition();
+/*
+Make (add code to class Tank),
+with fuel and different constructors
+*/
+
+        // At (0;0) fuel=100 
+        Tank justTank = new Tank();
+        justTank.goForward(200);
+        justTank.printPosition();
+        // At (10;20) fuel=100 
+//        Tank anywareTank = new Tank(10, 10);
+//        // At (20;30) fuel=200
+//        Tank customTank = new Tank(20, 30, 200);
+//        justTank.goForward(200);
+//        justTank.printPosition();
+//        anywareTank.goBackward(-200);
+//        anywareTank.printPosition();
+//        customTank.goForward(201);
+//        customTank.printPosition();
+       
+/* 
+This fragment of code has to output 
+
+The Tank is at 100, 0 now.
+The Tank is at 110, 10 now.
+The Tank is at 220, 30 now.
+
+*/
     }
+
 }
--- src/Tank.java
+++ src/Tank.java
@@ -2,12 +2,34 @@ public class Tank {
 
     int x, y;
     int dir;
-
+    int fuel = 100;
+    public Tank(){
+        this.fuel = fuel;
+        this.x = 0;
+        this.y = 0;
+    }
+    public Tank (int x, int y, int fuel){
+        this.fuel = fuel;
+        this.x = x;
+        this.y = y;
+    }
+    public Tank (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
     public void goForward(int i) {
-        if (dir == 0) x += i;
-        else if (dir == 1) y += i;
-        else if (dir == 2) x -= i;
-        else y -= i;
+        if(i < 0){
+            this.fuel *= -1;
+            if(i < this.fuel) i = this.fuel;
+            this.fuel += i;
+        } else if(i > 0){
+            if(i > this.fuel) i = this.fuel;
+            this.fuel -= i;
+        }
+            if (dir == 0) x += i;
+            else if (dir == 1) y += i;
+            else if (dir == 2) x -= i;
+            else y -= i;
     }
     public void goBackward(int i) {
         goForward(-i);
